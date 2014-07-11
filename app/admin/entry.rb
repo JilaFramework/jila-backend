d521cf984882bf1012c888b4b7036663c46ea1d8
Nathan Jones <thenathanjones@gmail.com>
2014-07-11 22:52:33 +0800

    @thenathanjones: Remove the show pages

--- app/admin/entry.rb
+++ app/admin/entry.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Entry do
 
+  actions :all, except: [:show]
+
   permit_params :entry_word, :word_type, :translation, :description, :published?
 
   form do |f|
@@ -15,22 +17,12 @@ ActiveAdmin.register Entry do
 
   index do
     column :entry_word do |entry|
-      link_to entry.entry_word, admin_entry_path(entry)
+      link_to entry.entry_word, edit_admin_entry_path(entry)
     end
     column :word_type
     column :translation
     column :description
     column :published?
     actions
-  end
-
-  show title: :entry_word do |entry|
-    attributes_table do
-      row :entry_word
-      row :word_type
-      row :translation
-      row :description
-      row :published?
-    end
   end
 end
