c36ef00a29c2363a652018139439b95b4e0498c7
Nathan Jones <thenathanjones@gmail.com>
2014-07-16 22:35:49 +0800

    @thenathanjones: Test the published? scope on the Entry

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Entry, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:published) { Entry.create entry_word: 'published', word_type: 'noun', translation: 'published', published?: true }
+  let!(:unpublished) { Entry.create entry_word: 'unpublished', word_type: 'noun', translation: 'unpublished', published?: false }
+
+  describe :published? do
+  	it 'should only return published records' do
+  		entries = Entry.published?
+
+      expect(entries).to_not include(unpublished)
+      expect(entries).to include(published)
+  	end
+  end
 end
