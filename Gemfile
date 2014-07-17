f81d69c14aee858c7a5f4e1fb2cc71d38df4b1df
Nathan Jones <thenathanjones@gmail.com>
2014-07-17 14:53:29 +0800

    @thenathanjones: Allow cross origin requests to the API endpoint

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 
+gem 'rack-cors'
+
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack/cors'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,5 +20,12 @@ module LanguageBackend
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '/api/*', :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
