d51265eb799c8780ca2bdcb7eeb6945a7c3183c9
Nathan Jones <thenathanjones@gmail.com>
2014-08-09 13:51:44 +0800

    @thenathanjones: Remove the leading slash on the cache action to expire for the namespace

--- app/observers/api_sweeper.rb
+++ app/observers/api_sweeper.rb
@@ -2,6 +2,6 @@ class ApiSweeper < ActionController::Caching::Sweeper
   observe Category, Entry
 
   def after_save record
-    expire_action(:controller => "/api/sync", :action => "all")
+    expire_action(:controller => "api/sync", :action => "all")
   end
 end 
