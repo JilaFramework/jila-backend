e40306b3d0c72db434bd41ffd1b229795e02724e
Nathan Jones <thenathanjones@gmail.com>
2014-08-11 13:47:31 +0800

    @thenathanjones: This feels icky, but works fine. Allow the sweeper to be explicitly disabled if need be, such as adding data via migrations or during tests.

--- app/observers/api_sweeper.rb
+++ app/observers/api_sweeper.rb
@@ -1,12 +1,21 @@
 class ApiSweeper < ActionController::Caching::Sweeper
   observe Category, Entry
 
+  @@disabled = false
+
+  def self.disabled= new_state
+    puts "Setting API sweeper state to disabled: #{new_state}"
+    @@disabled = new_state
+  end
+
   def after_save record
-    fragment_path = ActionController::Caching::Actions::ActionCachePath.new(self, {
-        controller: '/api/sync',
-        action: 'all',
-        format: 'json'
-    }, false).path
-    expire_fragment("#{fragment_path}.json")
+    unless @@disabled
+      fragment_path = ActionController::Caching::Actions::ActionCachePath.new(self, {
+          controller: '/api/sync',
+          action: 'all',
+          format: 'json'
+      }, false).path
+      expire_fragment("#{fragment_path}.json")
+    end
   end
 end 
--- db/migrate/20140729080040_make_common_category.rb
+++ db/migrate/20140729080040_make_common_category.rb
@@ -1,5 +1,7 @@
 class MakeCommonCategory < ActiveRecord::Migration
   def up
+    ApiSweeper.disabled = true
+
     if Category.count > 0
       puts "Existing category found with ID #1"
       first_category = Category.find_by_id 1
@@ -10,6 +12,8 @@ class MakeCommonCategory < ActiveRecord::Migration
     end
 
     new_category = Category.create id: 1, name: 'Common Phrases'
+
+    ApiSweeper.disabled = false
   end
 
   def down
