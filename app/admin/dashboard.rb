54fd5d1cd7ad4a68c4d229f8aec5887d9443af5b
Nathan Jones <thenathanjones@gmail.com>
2014-10-31 22:34:11 +1100

    @thenathanjones: Provide a simple dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,45 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+
+    columns do
+      column do
+        panel "Welcome to Jila!" do
+          para do
+            text_node "This is where you can administer the data for the"
+            b "Jila"
+            text_node "language application."
+          end
+          para do
+            text_node "You can create, edit, and view "
+            text_node link_to "categories", admin_categories_path
+            text_node " and "
+            text_node link_to "entries", admin_entries_path
+            text_node " via the menu links above."
+          end
+          para do
+            text_node link_to "Admin users", admin_admin_users_path
+            text_node " can also be managed from the links above."
+          end
+        end
+        panel "State" do
+          para do
+            text_node "There are"
+            b Category.count, style: "font-size: 1.5em"
+            text_node "categories in the dictionary."
+          end
+          para do
+            text_node "There are"
+            b Entry.count, style: "font-size: 1.5em"
+            text_node "entries in the dictionary."
+          end
+        end
+        panel "Brought to you by" do
+          image_tag("/assets/tw_logo.png", width: "100%", style: "margin-top: 15px;")
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
