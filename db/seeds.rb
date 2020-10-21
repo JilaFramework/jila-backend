914e4fec4d975486ce7c86915e8d21fd663a5434
Brad Nguyen <brad@192-168-1-103.tpgi.com.au>
2020-10-21 14:25:46 +1100

    [Harmeet,Brad] first run SQL insert using seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'active_record'
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +8,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+input_file = "db/lexique_dict.txt"
+csv_file = "db/parsed_lexique.csv"
+
+def save_to_db(entry)
+    print("entry", entry)
+    sql = "INSERT INTO entries(entry_word) VALUES('#{entry[0]}')"
+    print("Execute SQL", sql)
+    ActiveRecord::Base.connection.execute(sql)
+end
+
+File.delete(csv_file) if File.exist?(csv_file)
+line_num = 0
+
+CSV.open(csv_file, "w") do |csv|
+    File.open(input_file,:encoding => 'utf-8').each do |line|
+        #print "#{line_num += 1} #{line}"
+        if not (line.include? "Category")
+            line = line.gsub("\n", "Category: None\n")
+        end
+
+        result = line.scan(/(\S+)\W+\[(.+)\]\W+([^.]+)\.(.+)Category:\W+([^.]+)/)
+        actual_value = result.first()
+        save_to_db(actual_value)
+        csv << actual_value
+        # print result
+        print "\n\n"
+    end
+end
