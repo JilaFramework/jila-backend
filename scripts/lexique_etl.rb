ffa25242e4657de85355abbf17e3dfe1b82d2e82
Harmeet Sokhi <harmeet.sokhi@thoughtworks.com>
2020-10-21 11:28:22 +1100

    [Harmeet,Brad] Added encoding for input file and funcionality to upload to csv

--- scripts/lexique_etl.rb
+++ scripts/lexique_etl.rb
@@ -1,15 +1,24 @@
 
+#u
 input_file = "lexique_dict.txt"
+csv_file = "parsed_lexique.csv"
 
+File.delete(csv_file) if File.exist?(csv_file)
 line_num = 0
+require 'csv'
+CSV.open(csv_file, "w") do |csv|
+    File.open(input_file,:encoding => 'utf-8').each do |line|
+        #print "#{line_num += 1} #{line}"
+        if not (line.include? "Category")
+            line = line.gsub("\n", "Category: None\n")
+        end
 
-File.open(input_file).each do |line|
-    print "#{line_num += 1} #{line}"
-    if not (line.include? "Category")
-        line = line.gsub("\n", "Category: None\n")
+        result = line.scan(/(\S+)\W+\[(.+)\]\W+([^.]+)\.(.+)Category:\W+([^.]+)/)
+        actual_value = result.first()
+        csv << actual_value
+        print result
+        print "\n\n"
     end
-
-    result = line.scan(/(\S+)\W+\[(.+)\]\W+([^.]+)\.(.+)Category:\W+([^.]+)/)
-    print result
-    print "\n\n"
 end
+
+
