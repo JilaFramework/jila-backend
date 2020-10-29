e3ba8284997e1341c0f3716ecd81b327e0fdbe43
Joe Sustaric <joe.sustaric@thoughtworks.com>
2020-10-29 12:03:09 +1100

    fixing deprecations and warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # The Environment Var are specific to heroku bucketeer add on you can alter them 
+  # The Environment Var are specific to heroku bucketeer add on you can alter them
   # to point to other AWS credentials as you wish
   # Note: https is enforced on most mobile apps
   config.paperclip_defaults = {
