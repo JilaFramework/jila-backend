a17ec1f7f18ac6503e7cfe50dead79f6d240739a
Bruno Mazzo <bruno.mazzo@thoughtworks.com>
2021-09-27 17:00:59 +1000

    Add basic CSV importer for entries

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,85 +10,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_026_013_921) do
-  create_table 'active_admin_comments', force: :cascade do |t|
-    t.string 'namespace'
-    t.text 'body'
-    t.string 'resource_id', null: false
-    t.string 'resource_type', null: false
-    t.string 'author_type'
-    t.integer 'author_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.index %w[author_type author_id], name: 'index_active_admin_comments_on_author_type_and_author_id'
-    t.index ['namespace'], name: 'index_active_admin_comments_on_namespace'
-    t.index %w[resource_type resource_id], name: 'index_active_admin_comments_on_resource_type_and_resource_id'
+ActiveRecord::Schema.define(version: 2020_10_26_013921) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_id", null: false
+    t.string "resource_type", null: false
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table 'admin_users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.index ['email'], name: 'index_admin_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_admin_users_on_reset_password_token', unique: true
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.string 'image_file_name'
-    t.string 'image_content_type'
-    t.bigint 'image_file_size'
-    t.datetime 'image_updated_at'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'position'
-    t.boolean 'image_game_available?', default: true
-    t.boolean 'audio_game_available?', default: true
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "position"
+    t.boolean "image_game_available?", default: true
+    t.boolean "audio_game_available?", default: true
   end
 
-  create_table 'categories_entries', force: :cascade do |t|
-    t.integer 'category_id'
-    t.integer 'entry_id'
+  create_table "categories_entries", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "entry_id"
   end
 
-  create_table 'entries', force: :cascade do |t|
-    t.string 'entry_word'
-    t.string 'word_type'
-    t.text 'description'
-    t.boolean 'published?'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string 'image_file_name'
-    t.string 'image_content_type'
-    t.integer 'image_file_size'
-    t.datetime 'image_updated_at'
-    t.string 'audio_file_name'
-    t.string 'audio_content_type'
-    t.integer 'audio_file_size'
-    t.datetime 'audio_updated_at'
-    t.text 'extras', default: '{alternate_translations: []}'
-    t.integer 'display_order'
-    t.string 'pronunciation'
-    t.string 'meaning'
-    t.string 'example'
-    t.string 'example_translation'
+  create_table "entries", force: :cascade do |t|
+    t.string "entry_word"
+    t.string "word_type"
+    t.text "description"
+    t.boolean "published?"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
+    t.string "audio_file_name"
+    t.string "audio_content_type"
+    t.integer "audio_file_size"
+    t.datetime "audio_updated_at"
+    t.text "extras", default: "{alternate_translations: []}"
+    t.integer "display_order"
+    t.string "pronunciation"
+    t.string "meaning"
+    t.string "example"
+    t.string "example_translation"
   end
 
-  create_table 'image_credits', force: :cascade do |t|
-    t.string 'attribution_text'
-    t.string 'link'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'entry_id'
-    t.integer 'category_id'
+  create_table "image_credits", force: :cascade do |t|
+    t.string "attribution_text"
+    t.string "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "entry_id"
+    t.integer "category_id"
   end
+
 end
